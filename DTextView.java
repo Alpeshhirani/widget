72d6a6df74f756795f7ac265197aea8cfbed662e
Indian <alpeshhirania@gmail.com>
2017-11-15 16:00:08 +0530

    Update DTextView.java

--- DTextView.java
+++ DTextView.java
@@ -11,52 +11,15 @@ import com.hirani.R;
 import com.hirani.utils.FontUtils;
 
 
-/**
- * Created by divyeshshani on 12/09/16.
- */
 public class DTextView extends AppCompatTextView {
 
     public DTextView(Context context) {
         super(context);
-
     }
 
     public DTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-
         TextViewHelper.setTypeface(context, this, attrs);
     }
-
-    public static class TextViewHelper {
-
-        private static Typeface typeface = null;
-        private static int type;
-
-        public static void setTypeface(Context context, TextView textView, AttributeSet attrs) {
-
-            TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.DTextView);
-            try {
-                type = ta.getInt(R.styleable.DTextView_textFontFace, 1);
-
-                typeface = FontUtils.fontName(context, type);
-
-            } finally {
-                ta.recycle();
-            }
-
-
-            textView.setTypeface(typeface);
-        }
-
-        public static void setTypeface(Context context, TextView textView) {
-
-            if (typeface == null) {
-
-                typeface = FontUtils.fontName(context, 1);
-            }
-
-            textView.setTypeface(typeface);
-        }
-
-    }
 }
+
